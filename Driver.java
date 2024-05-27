29b9e7941f276b9d69490354f242962899ab0173
Zohair Syed <1syedzohair@gmail.com>
2024-05-27 14:38:00 -0400

    updated Polynomial.java & Driver.java

--- Driver.java
+++ Driver.java
@@ -4,7 +4,7 @@ public class Driver {
     public static void main(String[] args) 
     {
         Polynomial p = new Polynomial();
-        //System.out.println(p.evaluate(3));
+        System.out.println(p.evaluate(3));
 
         double[] c1 = { 6, 0, 0, 5 };
         int[] e1 = { 0, 1, 2, 3 };
@@ -13,11 +13,11 @@ public class Driver {
         double[] c2 = { 0, -2, 0, 0, -9 };
         int[] e2 = { 0, 1, 2, 3, 4 };
         Polynomial p2 = new Polynomial(c2, e2);
-        //Polynomial s = p1.add(p2);
-        //s.display();
+        Polynomial s = p1.add(p2);
+        s.display();
 
         Polynomial multiply = p1.multilply(p2);
-        //multiply.display();
+        multiply.display();
 
         Polynomial f = new Polynomial(new File("test.txt"));
         f.display();
